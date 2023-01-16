ef7680cecf8a708e5474b231d6bb228fd46a3333
PawarV <pawar@rumq.net>
2023-01-16 19:03:56 +0000

    save

--- book-code/corejava/v1ch06/pawarv/ConstructorRefTest.java
+++ book-code/corejava/v1ch06/pawarv/ConstructorRefTest.java
@@ -21,7 +21,8 @@ public class ConstructorRefTest {
         System.out.println("***************** peopleArray");
         //  The toArray method invokes this constructor to obtain an array of the correct type. 
         // Then it fills and returns the array.
-        Person[] peopleArray = names.stream().map(Person::new).toArray(Person[]::new);
+        var peopleArray = names.stream().map(Person::new).toArray(Person[]::new);
+        // Person[] peopleArray = names.stream().map(Person::new).toArray(Person[]::new);
         for (Person p : peopleArray) {
             System.out.println(p);
         }
