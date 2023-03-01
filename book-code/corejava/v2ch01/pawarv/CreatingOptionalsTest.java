1e53fb4a76b65b082e7736a6c7db0e76a7b696db
PawarV <pawar@rumq.net>
2023-03-01 19:13:49 +0000

    save

--- book-code/corejava/v2ch01/pawarv/CreatingOptionalsTest.java
+++ book-code/corejava/v2ch01/pawarv/CreatingOptionalsTest.java
@@ -7,22 +7,49 @@ public class CreatingOptionalsTest {
     public static void main(String[] args) {
         // Create an Optional with a value
         Optional<String> name = Optional.of("Adam");
-        System.out.println(name.isPresent());
-        System.out.println(name.isEmpty());
-        System.out.println(name.get());
+        System.out.println(name.isPresent()); // true
+        System.out.println(name.isEmpty()); // false
+        System.out.println(name.get());  // Adam
         System.out.println(name.orElse("No name found"));
 
-        // Create an Optional with a null value
-        // Optional<String> nameIsNull = Optional.ofNullable(null);
+        // Create an Optional with a null value        
         Optional<String> nameEmptyOptional = Optional.empty();
-        System.out.println(nameEmptyOptional.isPresent());
-        System.out.println(nameEmptyOptional.isEmpty());
+        System.out.println(nameEmptyOptional.isPresent()); // false
+        System.out.println(nameEmptyOptional.isEmpty()); // true
         // System.out.println(name2.get());
         // Can do this, but it's no better than checking for null;
         if (nameEmptyOptional.isPresent()) {
             System.out.println(nameEmptyOptional.get());
         }
 
+        // Can take a null value 
+        Optional<String> nameIsNull = Optional.ofNullable(null);
+        System.out.println(nameIsNull.isPresent()); // false
+
+        // Or take a non-null value
+        Optional<String> nameIsNotNull = Optional.ofNullable("Adam");
+        System.out.println(nameIsNotNull.isPresent()); // true
+
+        // Call a method that returns an Optional
+        Optional<String> nameFromMethod = getOptionalName("Adam");
+        System.out.println(nameFromMethod.isPresent()); // true
+        System.out.println(nameFromMethod.get()); // Adam
+
+        // Call a method that returns an Optional
+        Optional<String> nameFromMethod2 = getOptionalName(null);
+        System.out.println(nameFromMethod2.isPresent()); // false
+        System.out.println(nameFromMethod2.orElse("No name found"));
+
+
+    }
+
+    // A method that returns an Optional
+    public static Optional<String> getOptionalName(String name) {
+        if (name == null) {
+            return Optional.empty();
+        } else {
+            return Optional.of(name);
+        }        
     }
 
 }
--- pages/v2ch01Streams.md
+++ pages/v2ch01Streams.md
@@ -169,15 +169,19 @@ We can consume an Optional value using `ifPresent` , `ifPresentOrElse` methods. 
 
 See [PipeliningOptionalsTest](../book-code/corejava/v2ch01/pawarv/PipeliningOptionalsTest.java)
 
-We can use `map` and `flatMap` methods to transform an Optional value. It takes a Function as an argument. It returns an Optional object containing the transformed value, if any.
+We can use `map` method to transform an Optional value. It takes a Function as an argument. It returns an Optional object containing the transformed value, if any.
 
 ### 1.7.4 How Not to Work with Optional Values
 
 If you still use `isPresent` and then use the `get` method, you get no benefit from using Optional.
 
+See  
+- [CreatingOptionalsTest](../book-code/corejava/v2ch01/
+
 ### 1.7.5 Creating Optional Values
 
-See [CreatingOptionalsTest](../book-code/corejava/v2ch01/pawarv/CreatingOptionalsTest.java).
+See 
+- [CreatingOptionalsTest](../book-code/corejava/v2ch01/pawarv/CreatingOptionalsTest.java).
 
 ### 1.7.6 Composing Optional Value Functions with flatMap
 
