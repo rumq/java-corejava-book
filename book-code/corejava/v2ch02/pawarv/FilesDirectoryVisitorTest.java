c581f03b96203feab5e5534732e3a543f15c98ab
PawarV <pawar@rumq.net>
2023-03-17 19:04:35 +0000

    #2

--- book-code/corejava/v2ch02/pawarv/FilesDirectoryVisitorTest.java
+++ book-code/corejava/v2ch02/pawarv/FilesDirectoryVisitorTest.java
@@ -8,6 +8,10 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.stream.Stream;
 
+/**
+ *  Demonstrates how to use Files.walk, Files.find, Files.list, 
+ *  and Files.walkFileTree
+ */
 public class FilesDirectoryVisitorTest {
 
     public static void main(String[] args) throws IOException {
