62b2c51e0b005daa9c4840b537466e0c63eca294
PawarV <pawar@rumq.net>
2023-03-17 17:26:17 +0000

    save

--- book-code/corejava/v2ch02/pawarv/FilesHandlingTest.java
+++ book-code/corejava/v2ch02/pawarv/FilesHandlingTest.java
@@ -5,7 +5,8 @@ import static other.Constants.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-public class FilesCreatingTest {
+
+public class FilesHandlingTest {
     public static void main(String[] args) throws IOException {
 
         // exists ?
@@ -25,41 +26,39 @@ public class FilesCreatingTest {
         System.out.println("Is the directory a directory? " + result);
 
         // isRegularFile ?
-        result = Files.isRegularFile(Path.of(DATA_DIRECTORY, INDIAN_RUPEE_TXT ) );
-        System.out.println("Is the file a regular file? " + result);  
+        result = Files.isRegularFile(Path.of(DATA_DIRECTORY, INDIAN_RUPEE_TXT));
+        System.out.println("Is the file a regular file? " + result);
 
         // createFile
         Files.createFile(Path.of(DATA_DIRECTORY + "temp1.txt"));
         System.out.println("Created a file ? " + Files.exists(Path.of(DATA_DIRECTORY + "temp1.txt")));
 
         // copy
-        Files.copy(Path.of(DATA_DIRECTORY + INDIAN_RUPEE_TXT),  Path.of(DATA_DIRECTORY + "temp2.txt"));
+        Files.copy(Path.of(DATA_DIRECTORY + INDIAN_RUPEE_TXT), Path.of(DATA_DIRECTORY + "temp2.txt"));
 
         // move
         Files.move(Path.of(DATA_DIRECTORY + "temp1.txt"), Path.of(DATA_DIRECTORY + "temp3.txt"));
 
         // createDirectories
         Files.createDirectories(Path.of(DATA_DIRECTORY + "temp4/temp5/temp6"));
-        
+
         // createTempDirectory
         var tempDirectory = Files.createTempDirectory(Path.of(DATA_DIRECTORY), "test");
 
         // createTempFile
         var tempFile = Files.createTempFile(Path.of(DATA_DIRECTORY), "test", ".txt");
 
-
-        //cleanup
+        // cleanup
         Files.delete(Path.of(DATA_DIRECTORY + "temp4/temp5/temp6"));
         Files.delete(Path.of(DATA_DIRECTORY + "temp4/temp5"));
         Files.delete(Path.of(DATA_DIRECTORY + "temp4"));
-        
+
         Files.delete(tempDirectory);
         Files.delete(tempFile);
 
         Files.deleteIfExists(Path.of(DATA_DIRECTORY + "temp1.txt"));
         Files.deleteIfExists(Path.of(DATA_DIRECTORY + "temp2.txt"));
         Files.deleteIfExists(Path.of(DATA_DIRECTORY + "temp3.txt"));
-
 
     }
 }
